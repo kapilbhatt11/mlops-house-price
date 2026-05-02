dd891b8ee718985fcbe120317f5a47dc5b6cd3d3
kapilbhatt11 <kapil.bhatt111@gmail.com>
2026-05-02 14:15:22 +0530

    test commit final

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-print("Hello test tsing git bhai")
-print('Hello test tsing git bhai second line')
-print('kapil bhatt work')
-
-for i in range(3):
-    print(f"Iteration {i}")
-    
-print("This is a test file for git")
-print("pichla delete mar kar ye use kiya he dekhte he !")
-print("radhe radhe")
+print("testign done!")
